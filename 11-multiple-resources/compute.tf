e9fcb0a81cc37f9eccdc46ef7fb972440a0b4573
William Dutcher <wdutcher.ctr@silveredge-gs.com>
2025-08-19 09:57:10 -0400

    Complete 11

--- 11-multiple-resources/compute.tf
+++ 11-multiple-resources/compute.tf
@@ -2,10 +2,24 @@ resource "aws_instance" "from_list" {
   count         = length(var.ec2_instance_config_list)
   ami           = local.ami_ids[var.ec2_instance_config_list[count.index].ami]
   instance_type = var.ec2_instance_config_list[count.index].instance_type
-  subnet_id     = aws_subnet.main[count.index % length(aws_subnet.main)].id
+  subnet_id     = aws_subnet.main[var.ec2_instance_config_list[count.index].subnet_name].id
 
   tags = {
     Name    = "${local.project}-${count.index}"
+    Project = local.project
+  }
+}
+
+resource "aws_instance" "from_map" {
+  # each.key => holds key of map
+  # each.value => holds value of map
+  for_each      = var.ec2_instance_config_map
+  ami           = local.ami_ids[each.value.ami]
+  instance_type = each.value.instance_type
+  subnet_id     = aws_subnet.main[each.value.subnet_name].id
+
+  tags = {
+    Name    = "${local.project}-${each.key}"
     Project = local.project
   }
 }
--- 11-multiple-resources/variables.tf
+++ 11-multiple-resources/variables.tf
@@ -1,18 +1,73 @@
-variable "subnet_count" {
-  type    = number
-  default = 2
-}
+# variable "subnet_count" {
+#   type    = number
+#   default = 2
+# }
 
-variable "ec2_instance_count" {
-  type    = number
-  default = 1
+# variable "ec2_instance_count" {
+#   type    = number
+#   default = 1
+# }
+
+variable "subnet_config" {
+  type = map(object({
+    cidr_block = string
+  }))
+
+  # Ensure that all provided CIDR blocks are valid
+  validation {
+    condition = alltrue([
+      for config in values(var.subnet_config) : can(cidrnetmask(config.cidr_block))
+    ])
+    error_message = "At least one of the provided CIDR blocks is not valid."
+  }
 }
 
 variable "ec2_instance_config_list" {
   type = list(object({
     instance_type = string
     ami           = string
+    subnet_name   = optional(string, "default")
   }))
 
   default = []
+
+  # Ensure only t2.micro is used
+  validation {
+    condition = alltrue([
+      for config in var.ec2_instance_config_list : contains(["t2.micro"], config.instance_type)
+    ])
+    error_message = "Only t2.micro instances are allowed."
+  }
+
+  # Ensure only ubuntu and nginx images are used
+  validation {
+    condition = alltrue([
+      for config in var.ec2_instance_config_list : contains(["ubuntu", "nginx"], config.ami)
+    ])
+    error_message = "Only \"ubuntu\" and \"nginx\" ami values are allowed."
+  }
+}
+
+variable "ec2_instance_config_map" {
+  type = map(object({
+    instance_type = string
+    ami           = string
+    subnet_name   = optional(string, "default")
+    # subnet_index  = optional(number, 0)
+  }))
+  # Ensure only t2.micro is used
+  validation {
+    condition = alltrue([
+      for key, config in var.ec2_instance_config_map : contains(["t2.micro"], config.instance_type)
+    ])
+    error_message = "Only t2.micro instances are allowed."
+  }
+
+  # Ensure only ubuntu and nginx images are used
+  validation {
+    condition = alltrue([
+      for config in values(var.ec2_instance_config_map) : contains(["ubuntu", "nginx"], config.ami)
+    ])
+    error_message = "Only \"ubuntu\" and \"nginx\" ami values are allowed."
+  }
 }
